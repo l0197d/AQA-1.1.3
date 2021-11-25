b3538bfb9203b9b15f1cfcba9e89ab7e9d7a2c00
l0197d <titus04@yandex.ru>
2021-11-26 00:19:33 +0300

    v1.6

--- src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -2,6 +2,8 @@ package ru.netology.rest;
 
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 
 import static io.restassured.RestAssured.given;
 
@@ -16,11 +18,12 @@ class MobileBankApiTestV2 {
       .when()
           .get("/demo/accounts")
       // Проверки
-      .then()
-          .statusCode(200)
-          // .header("Content-Type", "application/json; charset=UTF-8")
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
+              .then()
+              .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+              // .header("Content-Type", "application/json; charset=UTF-8")
+              // специализированные проверки - лучше
+              .contentType(ContentType.JSON)
       ;
     }
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+
 
 class MobileBankApiTestV3 {
     @Test
